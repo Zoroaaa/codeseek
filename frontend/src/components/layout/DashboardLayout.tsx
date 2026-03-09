8d09b00aba57b3cd5bba355d04ef694a12cfc8c1
Zoroaaa <1121896694@qq.com>
2026-03-10 01:01:38 +0800

    cimmit

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -38,7 +38,7 @@ const navItems: NavItem[] = [
 
 export const DashboardLayout: React.FC = () => {
   const { user, logout } = useAuthStore();
-  const { theme, resolvedTheme, toggleTheme, sidebarCollapsed, toggleSidebar } = useThemeStore();
+  const { resolvedTheme, toggleTheme, sidebarCollapsed, toggleSidebar } = useThemeStore();
   const location = useLocation();
   const navigate = useNavigate();
 
