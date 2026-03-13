9f040055e62875b35829d384494ccef39fdbb7db
Zoroaaa <1121896694@qq.com>
2026-03-13 19:55:57 +0800

    cimmit

--- frontend/src/pages/MainSearchPage.tsx
+++ frontend/src/pages/MainSearchPage.tsx
@@ -583,20 +583,18 @@ export const MainSearchPage: React.FC = () => {
 
           </div>
 
-          <div className="hidden lg:flex lg:flex-col gap-3 sm:gap-4">
+          <div className="hidden lg:block space-y-3 sm:space-y-4">
             <QuickActionsPanel isAdmin={isAdmin} communityEnabled={communityEnabled} />
             {isAuthenticated && (
-              <div className="flex-1 flex flex-col">
-                <FavoritesPanel
-                  favorites={favorites}
-                  isLoading={isLoadingFavorites}
-                  show={showFavorites}
-                  isProxyEnabled={isProxyEnabled}
-                  onToggle={() => setShowFavorites(!showFavorites)}
-                  onRemove={handleRemoveFavorite}
-                  onExport={handleExportFavorites}
-                />
-              </div>
+              <FavoritesPanel
+                favorites={favorites}
+                isLoading={isLoadingFavorites}
+                show={showFavorites}
+                isProxyEnabled={isProxyEnabled}
+                onToggle={() => setShowFavorites(!showFavorites)}
+                onRemove={handleRemoveFavorite}
+                onExport={handleExportFavorites}
+              />
             )}
           </div>
 
